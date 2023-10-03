b47bd21a060d4f324a176899b60f9ef21836c514
Van Chi Nguyen <nguyenvanchi210@gmail.com>
2023-10-03 13:52:37 +0700

    update

---
+++ app.js
@@ -0,0 +1,11 @@
+function openMenu() {
+  document.getElementById('mobileMenu').style.display = 'flex';
+}
+function closeMenu() {
+  document.getElementById('mobileMenu').style.display = 'none';
+}
+
+function toggleMenu() {
+  document.getElementById('mobileMenu').classList.toggle = 'animate__slideInRight';
+  document.getElementById('mobileMenu').classList.toggle = 'animate__slideOutRight';
+}
